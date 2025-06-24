f5d2b3a3e3d0a4257bcd2a426849dddd8b9c0407
Joyce S. Lima <jsarmentodelima@gmail.com>
2025-06-24 16:50:52 -0300

    Exercicios feitos durante a aula de LP2

--- ecommerce-minas/main.js
+++ ecommerce-minas/main.js
@@ -1,11 +1,21 @@
 // 1. Frete grátis 
 function calculaFrete() {
-  const total = Number(document.getElementById('totalInput').value);
+  const valorTotal = Number(document.getElementById('totalInput').value);
   let mensagem;
 
   // Se o total for maior ou igual a 150, a mensagem deve ser 'Frete grátis! 🎉'
-  // Se o total for maior ou igual a 0 (mas menor que 150), a mensagem deve ser 'Frete: R$ 20.00 😅'
+if (valorTotal >= 150) {
+  mensagem = "Frete grátis! 🎉"  
+} else if (valorTotal < 20) {
+  // Se o total for menor que vinte, então o pedido mínimo não foi atingido enão tem frete disponivel
+  mensagem = "Pedido mínimo não foi atingido - Frete indisponivel"
+} else if (valorTotal >= 20) {
+  // Se o total for maior ou igual a 20 (mas menor que 150), a mensagem deve ser 'Frete: R$ 20.00 😅'
+  mensagem = "Frete: R$ 20.00 😅"
+} else {
   // Se o valor for negativo ou inválido, a mensagem deve ser 'Valor inválido.'
+  mensagem = "Valor inválido."
+}
 
   document.getElementById('shippingResult').textContent = mensagem;
 }
@@ -14,25 +24,43 @@ function calculaFrete() {
 function mensagemDeBoasVindas() {
   const tipo = document.getElementById('userInput').value;
   let mensagem;
+  switch (tipo) {
+    case "convidada":
+      mensagem = "Oi, visitante! Crie uma conta pra ganhar descontos.";
+      break;
+    case "registrada":
+      mensagem = "Bem-vinda de volta!";
+      break;
+    case "premium":
+      mensagem = "Olá, cliente premium! Frete grátis garantido.";
+      break;
+    default:
+      mensagem = "Tipo desconhecido.";
+      break;
+  }
 
   // Usar switch case para definir a mensagem com base no tipo:
-  // - 'guest': "Oi, visitante! Crie uma conta pra ganhar descontos."
-  // - 'registered': "Bem-vinda de volta!"
+  // - 'convidada': "Oi, visitante! Crie uma conta pra ganhar descontos."
+  // - 'registrada': "Bem-vinda de volta!"
   // - 'premium': "Olá, cliente premium! Frete grátis garantido."
   // - 'admin': "Admin logado. Painel disponível."
   // Qualquer outro valor: "Tipo desconhecido."
 
   document.getElementById('userResult').textContent = mensagem;
 }
-
 // 3. Pode finalizar a compra?
 function podeFazerCheckout() {
   const items   = document.getElementById('hasItems').checked;
   const pagamento = document.getElementById('paymentOk').checked;
   let mensagem;
-
   // Se os dois forem verdadeiros (items e pagamento), a mensagem deve ser 'Compra liberada 🚀'
   // Caso contrário, deve ser 'Compra bloqueada. Verifique carrinho/pagamento.'
+
+  if (items && pagamento) {
+    mensagem = "Compra liberada 🚀"
+  } else {
+    mensagem = "Compra bloqueada. Verifique carrinho/pagamento."
+  }
 
   document.getElementById('checkoutResult').textContent = mensagem;
 }
@@ -45,6 +73,12 @@ function temDescontoEspecial() {
 
   // Se pelo menos um for verdadeiro (primeiraCompra ou cupomVip), a mensagem deve ser 'Desconto especial aplicado! 🎁'
   // Caso contrário, deve ser 'Desconto não disponível.'
+
+  if (primeiraCompra || cupomVip) {
+      mensagem = 'Desconto especial aplicado! 🎁';
+  } else {
+    mensagem = 'Desconto não disponível.'
+  }
 
   document.getElementById('specialResult').textContent = mensagem;
 }
